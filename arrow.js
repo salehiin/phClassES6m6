739bed8d2562f6016832f2ee8a289be6a5974362
salehiin <salehin.ay@gmail.com>
2024-09-16 21:44:42 +0600

    more arrow function and spread operator

--- arrow.js
+++ arrow.js
@@ -27,3 +27,45 @@ const sum3 = add3(5, 90);
 console.log(sum3);
 const multi = add4(1, 2, 3, 4);
 console.log(multi);
+
+
+// 30.5
+const difference = (x, y) => x - y;
+const multiply = (first, second, third) => first * second * third;
+
+const getAge = (person) => person.age; // object - as one parameter
+const student = { name: 'ananta jalil', age: 40 };
+const age = getAge(student);
+console.log(age);
+
+const speaker = { brand: 'JBL', price: 49, color: 'blue', size: 'small' }
+const getPrice = (product) => product.price;
+const price = getPrice(speaker);
+console.log(price);
+const getBrand = (product) => product.brand;
+const brand = getBrand(speaker);
+console.log(brand)
+const getColor = (product) => product.color;
+const color = getColor(speaker)
+console.log(color);
+const getSize = (product) => product.size;
+const size = getSize(speaker);
+console.log(size);
+
+const getThird = numbers => numbers[2];
+const third = getThird([5, 9, 88, 2, 13]);
+console.log(third);
+
+const doubleIt = num => num * 2;  //single parameter - bracket not mandatory
+
+// no parameter
+const getPI = () => Math.PI;
+console.log(getPI());
+
+// Large arrow function. return keyword mandatory.
+const doMath = (x, y, z) => {
+    const sum = x + y + z;
+    const mullt = x * y * z;
+    const result = sum + mult;
+    return result;
+}
---
+++ spread.js
@@ -0,0 +1,22 @@
+const max = Math.max(6, 23, 45, 1, 89, 23);
+const numbers = [3, 5, 2, 45, 5, 43, 90, 32, 15]
+const arrayMax = Math.max(...numbers);
+// console.log(...numbers)
+console.log(arrayMax)
+
+// use spread operator to copy
+const nums = [4, 5, 87, 9];
+const nums2 = nums;
+// nums2.push(12);
+// console.log(nums); // when set value from one variable to another variable - non-primitive data type preserve the referrence
+// console.log(nums2);
+const nums3 = [...nums]
+console.log(nums3);
+nums.push(100)
+console.log(nums3)
+console.log(nums)
+
+// advanced 
+const sonkha = [...nums, 9999] // spread operator use to copy an array
+console.log(sonkha);
+ 
